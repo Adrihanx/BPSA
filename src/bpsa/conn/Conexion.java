d379211ae03967299c55ce97476b7699adf176de
alanantonio <alanantonio@Adrihanx>
2017-07-08 19:09:03 -0500

    #2-> Agregando los jar y el JDBC. Creando la conexion

---
+++ src/bpsa/conn/Conexion.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Programa diseñado integramente para la empresa BPSA por Cardenal Bridge Company
+ *
+ */
+
+package bpsa.conn;
+
+/**
+ *
+ * @author alanantonio
+ */
+public class Conexion {
+String connectionUrl = "jdbc:sqlserver://localhost:1433;" +
+            "databaseName=pubs;user=sa; password=adminadmin;"; 
+}
